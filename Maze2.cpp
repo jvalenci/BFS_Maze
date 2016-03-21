7cc80e7e11adcd3533df91d77e1a9a14f41a38a9
Jonathan Valencia <darkmanp2@yahoo.com>
2016-03-21 11:30:49 -0400

    Breaks if there is no way out.

--- Maze2.cpp
+++ Maze2.cpp
@@ -9,7 +9,34 @@ void Maze2::find_exit()
    // Use the BFS method to fill the array of predecessors
   Position nbr, current;
   direction d;
+  int count = 0;
 
+  
+  Q.push(start);
+  M[start.row][start.col] = VISITED;
+
+  //check to make sure initi starting pos isn't blocked in
+  for (direction dir = DOWN; dir != NONE; dir = next_direction(dir)) {
+	  if (M[start.Neighbor(dir).row][start.Neighbor(dir).col] != OPEN) {
+		  ++count;
+		  continue;
+	  }
+	  break;
+  }
+
+  //if blocked it will return
+  if (count >= 4) {
+	  Q.pop();
+	  return;
+  }
+
+  while (!Q.empty())
+  {
+	  current = Q.front();
+	  Q.pop();
+
+
+  }
 }
 
 
