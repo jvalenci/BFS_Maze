d2abbc9956f27faaf74abbe014b0aaa844688bf9
Jonathan Valencia <darkmanp2@yahoo.com>
2016-03-21 12:16:09 -0400

    finished and works.

--- Maze2.cpp
+++ Maze2.cpp
@@ -9,32 +9,17 @@ void Maze2::find_exit()
    // Use the BFS method to fill the array of predecessors
   Position nbr, current;
   direction d;
-  int count = 0;
-  //bool foundExit = false;
   
   Q.push(start);
   M[start.row][start.col] = VISITED;
 
-  //check to make sure initi starting pos isn't blocked in
-  for (direction dir = DOWN; dir != NONE; dir = next_direction(dir)) {
-	  if (M[start.Neighbor(dir).row][start.Neighbor(dir).col] != OPEN) {
-		  ++count;
-		  continue;
-	  }
-	  break;
-  }
-
-  //if blocked it will return
-  if (count >= 4) {
-	  Q.pop();
-	  return;
-  }
 
   while (!Q.empty())
   {
-	  current = Q.front();
-	  Q.pop();
+	  current = Q.front();			  //push current to the queue
+	  Q.pop();						//pop off the first element
 
+	  //main logic if open and not exit push on queue and track predessor
 	  for (d = DOWN; d != NONE; d = next_direction(d)) {
 		  nbr = current.Neighbor(d);
 		  if (M[nbr.row][nbr.col] == OPEN) {
@@ -42,14 +27,12 @@ void Maze2::find_exit()
 			  pred[nbr.row][nbr.col] = current;
 
 			  if (nbr == exitpos) {
-				  //foundExit = true;
 				  return;
 			  }
 
 			  Q.push(nbr);
 		  }
 	  }
-
   }
 }
 
